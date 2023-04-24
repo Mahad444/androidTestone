d593f76d2f5e75e210d19d9415ab31bb7a5d93a5
Ally Juma Said <mahadsaid95@icloud.com>
2023-04-24 22:36:03 +0100

    learning dataTypes

--- app/src/main/java/muhib/com/mahad.kt
+++ app/src/main/java/muhib/com/mahad.kt
@@ -1,5 +1,20 @@
 
 
 fun main (args:Array<String>){
+//    CONSTANTS
+    val numbeer : Int = 10        //Int: stores interger number
+    val pi : Double = 3.14234453    //Double: stores this is double variable i.e decimals with many characters
+    val k : Float = 45.8797f      //Float: stores float numbers decimals eith few charcters
+    val nameOfMe : String = "Mahad Said"
+
+    // VARIABLES
+    var  numberOfInit:Int = 77
+    var nameOfMine : String = "Mahad Said"
+    var Temperature : Float = 30.6668896f
+
+
+
+
     println("Hello World Kotlin")
+    println(numbeer)
 }
