9d2fed3ca5516ee67f9d8a99cecea31bca112f2c
Ally Juma Said <mahadsaid95@icloud.com>
2023-04-26 00:31:00 +0100

    learning strings template and concatination

---
+++ app/src/main/java/muhib/com/numbers.kt
@@ -0,0 +1,44 @@
+fun  main(){
+    val s:Short = 12
+    val b:Byte = 120
+    val i : Int = -10000
+    val l:Long = 133435346
+    val f:Float = 23.45637f
+
+    val d :Double = 457.7889
+
+    val answer = s+b+i+l+f
+
+    println(answer)
+
+//    characters
+    var letter:Char
+    letter = 'a'
+    println(letter)
+// Booleans
+
+    val flags :Boolean = true
+    val flagss : Boolean = false
+
+    println(flags)
+    println(flagss)
+
+//    Strings
+    val neno = "hello from Mahad Said here i am "
+    println(neno)
+
+//    stringConcatination
+    val stringA : String = "Hi "
+    val stringB : String = "friends"
+
+    println( stringA +stringB )
+
+//    STRING TEMPLATE
+    val stringF: String = "Khadija"
+    val stringL : String = "Hello my Name is $stringF"
+
+    println(stringL)
+
+
+
+}
