78eccafb48101e95978fce9268eb32a57f928fe3
Ally Juma Said <mahadsaid95@icloud.com>
2023-05-24 23:33:38 +0100

    learning about Classes,objects,Methods and properties with examples

---
+++ app/src/main/java/mahad/example/MainKotlinC.kt
@@ -0,0 +1,19 @@
+package mahad.example
+
+fun main () {
+
+    val student = Student()
+
+    student.name = "Mahad Said"
+    student.marks = 67
+
+    student.printMarks()
+
+    val student1 = Student()
+
+    student1.name = "Arafat"
+    student1.marks = 80
+    student1.printMarks()
+
+
+}
