2e9fb26154f1459f260568b6eefe000a105a1def
Ally Juma Said <mahadsaid95@icloud.com>
2023-05-25 00:06:59 +0100

    learning about constructors and init method and secondary constructors

--- app/src/main/java/mahad/example/Student.kt
+++ app/src/main/java/mahad/example/Student.kt
@@ -1,12 +1,27 @@
 package mahad.example
 
-class Student {
-    var name : String = ""
-    var marks = 0
+class Student (var name:String = "", var marks:Int = 0){
+//    var name : String = name
+//    var marks = marks
+
+    var nameOfTheClass : String = ""
+
+    constructor(nameOfTheClass: String):this(){
+        this.nameOfTheClass = nameOfTheClass
+    }
+
+
+    init {
+//        println("Init Block Student class $name class $nameOfTheClass and $ marks")
+    }
+//  var nameOfTheClass = "8B"  we cannot declare the accessed variable before the init block like this
+    init {
+//        println("Done Printing")
+    }
 
 
 //    Methods
     fun printMarks(){
-    println("Student $name has archieved $marks marks in the exam")
+    println("Student $name has archieved $marks marks in the exam and he is in class $nameOfTheClass")
     }
 }
