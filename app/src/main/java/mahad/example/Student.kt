553f3320c5f2f9caf13d7b3bb4762e090bc9249a
Ally Juma Said <mahadsaid95@icloud.com>
2023-05-24 23:21:47 +0100

    learning about Classes,objects,Methods and properties

---
+++ app/src/main/java/mahad/example/Student.kt
@@ -0,0 +1,12 @@
+package mahad.example
+
+class Student {
+    var name : String = ""
+    var marks = 0
+
+
+//    Methods
+    fun printMarks(){
+    println("Student $name has archieved $marks marks in the exam")
+    }
+}
